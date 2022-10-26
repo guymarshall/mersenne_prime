c4910a0a27bd8070b46cc0b4bb5f978b24cffbea
Guy Marshall <guymarshall9@gmail.com>
2022-10-26 23:50:47 +0100

    Initial commit with instructions

---
+++ app.py
@@ -0,0 +1,14 @@
+# make list to check
+    # square the previous number then subtract 2
+        # 4, 14 etc
+
+# do 2^n - 1
+# if the (n-1)th element of the list is a multiple of the answer, then PRIME else NOT PRIME
+# 2 -> 14, is it a multiple of 2^3 - 1 (7), if it is then PRIME else NOT PRIME
+# to get element of list, remember to take one away, so 14 is the 2nd number, but the 1st element of the list
+
+def main():
+    print("Running")
+
+if __name__ == "__main__":
+    main()
