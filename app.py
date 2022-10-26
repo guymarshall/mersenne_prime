a743302eff9af0f5d98b66ae7349c49fcf234630
Guy Marshall <guymarshall9@gmail.com>
2022-10-26 23:55:05 +0100

    Add code for generating multiples list

--- app.py
+++ app.py
@@ -7,8 +7,18 @@
 # 2 -> 14, is it a multiple of 2^3 - 1 (7), if it is then PRIME else NOT PRIME
 # to get element of list, remember to take one away, so 14 is the 2nd number, but the 1st element of the list
 
+def generate_list(count):
+    multiples = [4]
+
+    for _ in range(count - 1):
+        multiples.append((multiples[-1] ** 2) - 2)
+    
+    return multiples
+
 def main():
-    print("Running")
+    number = int(input("Enter number of elements in list: "))
+    multiples = generate_list(number)
+    print(multiples)
 
 if __name__ == "__main__":
     main()
